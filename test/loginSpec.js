9dd849b0ad73c8ffde7bf2b6ede25e85461198fb
Karl Groves <karl@karlgroves.com>
2016-10-28 18:16:42 -0400

    Comment out 2nd test

--- test/loginSpec.js
+++ test/loginSpec.js
@@ -13,24 +13,18 @@ describe('Login', function() {
     assert(title === 'Login');
   });
 
-  it('should log in', function() {
-    console.log(browser.getHTML('#login-form'));
+  //it('should log in', function() {
+  //  console.log(browser.getHTML('#login-form'));
+  //  browser.setValue('#email', 'me@example.com');
+  //  var eValue = browser.getValue('#email');
+  //  assert(eValue === 'me@example.com');
+  //  browser.setValue('#password', 'XYZPDQ123!');
+  //  var pValue = browser.getValue('#password');
+  //  assert(pValue === 'XYZPDQ123!');
+  //  browser.click('#submit');
+  //  browser.pause(100);
 
-    browser.setValue('#email', 'me@example.com');
-
-    var eValue = browser.getValue('#email');
-    assert(eValue === 'me@example.com');
-
-    browser.setValue('#password', 'XYZPDQ123!');
-
-    var pValue = browser.getValue('#password');
-    assert(pValue === 'XYZPDQ123!');
-
-    browser.click('#submit');
-
-    browser.pause(100);
-
-    var title = browser.getTitle();
-    assert(title === 'Projects');
-  });
+  //var title = browser.getTitle();
+  //  assert(title === 'Projects');
+  //});
 });
